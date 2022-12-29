8465df7548962d81bb40bdab461119c754564fa1
kirillpomazov <kirillpomazov@gmail.com>
2022-12-29 14:39:12 +0300

    elements added

---
+++ conftest.py
@@ -0,0 +1,13 @@
+import pytest
+from selenium import webdriver
+# from webdriver_manager.chrome import ChromeDriverManager
+
+
+@pytest.fixture(scope='function')
+def driver():
+    driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
+
+
+
