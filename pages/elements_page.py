65b50af2904dbcefaf1c008e5789d4b4b1506187
kirillpomazov <kirillpomazov@gmail.com>
2022-12-29 16:53:59 +0300

    test for form

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -13,3 +13,10 @@ class TextBoxPage(BasePage):
         self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys('sdasd')
         self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys('asdsa')
         self.element_is_visible(self.locators.SUBMIT).click()
+
+    def check_filled_form(self):
+        full_name = self.element_is_present(self.locators.CREATED_FULL_NAME).text.split(':')[1]
+        email = self.element_is_present(self.locators.CREATED_EMAIL).text.split(':')[1]
+        current_address = self.element_is_present(self.locators.CREATED_CURRENT_ADDRESS).text.split(':')[1]
+        permanent_address = self.element_is_present(self.locators.CREATED_PERMANENT_ADDRESS).text.split(':')[1]
+        return full_name, email, current_address, permanent_address
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -5,9 +5,12 @@ import time
 class TestElements:
     class TestTextBox:
 
-
-        def text_text_box(self, driver):
+        def test_text_box(self, driver):
             text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
             text_box_page.open()
             text_box_page.fill_all_fields()
-            time.sleep(5)
+            output_name, output_email, output_address, output_per_address = text_box_page.check_filled_form()
+            print(output_name)
+            print(output_email)
+            print(output_address)
+            print(output_per_address)
