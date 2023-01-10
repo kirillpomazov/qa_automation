aba4302f5fceef723699b84ef5cbed7880b616fc
kirillpomazov <kirillpomazov@gmail.com>
2023-01-10 14:59:02 +0300

    faker added

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -8,9 +8,9 @@ class TestElements:
         def test_text_box(self, driver):
             text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
             text_box_page.open()
-            text_box_page.fill_all_fields()
+            full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
             output_name, output_email, output_address, output_per_address = text_box_page.check_filled_form()
-            print(output_name)
-            print(output_email)
-            print(output_address)
-            print(output_per_address)
+            assert full_name == output_name
+            assert email == output_email
+            assert current_address == output_address
+            assert permanent_address == output_per_address
